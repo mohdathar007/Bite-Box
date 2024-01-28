62529d7a62c61e6b872210a27bbb1704ddca7378
Athar <mohdathar8888@gmail.com>
2024-01-28 23:47:00 +0530

    feat:added bill details section

--- src/customer/pages/Cart/Cart.jsx
+++ src/customer/pages/Cart/Cart.jsx
@@ -13,9 +13,40 @@ const Cart = () => {
 
              </div>
             <Divider/>
-            <div>
-
+            <div className='billDetails px-5 text-sm'>
+              <p className='font-extralight py-5'>
+                Bill Details
+              </p>
+              <div className='space-y-3'>
+                <div className='flex justify-between text-gray-400'>
+                  <p>Item Total</p>
+                  <p>₹949</p>
+                </div>
+                <div className='flex justify-between text-gray-400'>
+                  <p>Deliver Fee Total</p>
+                  <p>₹21</p>
+                </div>
+                <div className='flex justify-between text-gray-400'>
+                  <p>Plateform fee</p>
+                  <p>₹9</p>
+                </div>
+                <div className='flex justify-between text-gray-400'>
+                  <p>GST and Restaurant Charges</p>
+                  <p>₹33</p>
+                </div>
+                <Divider/>
+                <div className='flex justify-between text-gray-400'>
+                  <p>Total Pay</p>
+                  <p>₹1349</p>
+                </div>
+                
+              </div>
             </div>
+            </section>
+            <Divider orientation='vertical' flexItem/>
+
+            <section className='lg:w-[70%] px-5'>
+            
             </section>
             
         </main>
